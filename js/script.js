b2a1f841c7672023154afd9707cca0bea1c1abd1
xrenolomatel <eakhomiak@gmail.com>
2019-03-06 16:15:48 +0200

    end footer

---
+++ js/script.js
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        navText: [ ' ', ' ' ],
+
+        responsive:{
+            0:{
+                items:3
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
+});
+
